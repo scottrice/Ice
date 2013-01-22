0c34e26dfcef7f4b8a4796e45dc2ea026ab1b72a
Scott Rice <meris608@gmail.com>
2013-01-22 01:34:25 -0600

    Added an equality method to IceROM

--- ice/IceROM.py
+++ ice/IceROM.py
@@ -28,6 +28,9 @@ class ROM:
 
     def __repr__(self):
         return self.name()
+
+    def __eq__(self,other):
+        return self.path == other.path and self.console == other.console
         
     def name(self):
         name_with_ext = os.path.basename(self.path)
