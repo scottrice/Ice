92fccafcd517fe07e27878ee28efc99bad388f2d
Scott Rice <meris608@gmail.com>
2013-04-25 23:50:48 -0500

    Adds Gens support for custom controls (#7)
    Gens will now set it's control scheme based on the controls.txt file.
    This one is a little weird, as there isnt really anything to support
    that the controls in there are the correct controls for an Xbox 360
    controller. Hopefully by figuring out what those numbers mean, I can
    also figure out how Project 64 stores it's configuration

--- ice/emulators/wingens.py
+++ ice/emulators/wingens.py
@@ -15,7 +15,6 @@ import downloaded_emulator
 class WinGens(downloaded_emulator.DownloadedEmulator):
     
     _download_location_ = "https://dl.dropbox.com/u/2862706/ice_emulators/WinGens.zip"
-    # _relative_exe_path_ = os.path.join("WinGens","gens.exe")
     _relative_exe_path_ = os.path.join("WinGens","gens-launcher.exe")
     
     def __init__(self,console_name):
@@ -28,3 +27,30 @@ class WinGens(downloaded_emulator.DownloadedEmulator):
         "C:\Path\To\gens" "C:\Path\To\ROM"
         """
         return "\"%s\" \"%s\"" % (self.location,rom.path)
+        
+    def config_path(self):
+        return os.path.join(self.directory,"gens.cfg")
+        
+    def set_control_scheme(self,controls):
+        control_map = self.identifier_to_control_map()
+        def replacement_function(line):
+            try:
+                control_identifier = line[:line.index('=')]
+                control = control_map[control_identifier]
+                control_value = controls[control]
+                return "%s=%s\n" % (control_identifier,control_value)
+            except:
+                return line
+        self.replace_contents_of_file(self.config_path(),replacement_function)
+        
+    def identifier_to_control_map(self):
+        return {
+            "P1.Up":"up",
+            "P1.Down":"down",
+            "P1.Left":"left",
+            "P1.Right":"right",
+            "P1.Start":"start",
+            "P1.A":"a",
+            "P1.B":"b",
+            "P1.C":"c"
+        }
