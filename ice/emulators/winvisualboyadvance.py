aa51c34d1562b1462a9ac14033a97dcef4a1d7b7
Scott Rice <meris608@gmail.com>
2013-04-26 00:17:49 -0500

    Added VisualBoyAdvance custom control support (#7)

--- ice/emulators/winvisualboyadvance.py
+++ ice/emulators/winvisualboyadvance.py
@@ -43,3 +43,32 @@ class WinVisualBoyAdvance(downloaded_emulator.DownloadedEmulator):
         "C:\Path\\to\VisualBoyAdvance" "C:\Path\\to\ROM"
         """
         return "\"%s\" \"%s\"" % (self.location,rom.path)
+    
+    def config_path(self):
+        return os.path.join(self.directory,"vba.ini")
+
+    def set_control_scheme(self,controls):
+        control_map = self.identifier_to_control_map()
+        def replacement_function(line):
+            try:
+                control_identifier = line[:line.index('=')]
+                control = control_map[control_identifier]
+                control_value = controls[control]
+                return "%s=%s\n" % (control_identifier,control_value)
+            except:
+                return line
+        self.replace_contents_of_file(self.config_path(),replacement_function)
+
+    def identifier_to_control_map(self):
+        return {
+            "Joy0_Up":"up",
+            "Joy0_Down":"down",
+            "Joy0_Left":"left",
+            "Joy0_Right":"right",
+            "Joy0_A":"a",
+            "Joy0_B":"b",
+            "Joy0_L":"l",
+            "Joy0_R":"r",
+            "Joy0_Select":"select",
+            "Joy0_Start":"start",
+        }
