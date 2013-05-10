f4a8adbcb39196bcb87bafa85de81551902b77d8
Scott Rice <meris608@gmail.com>
2013-05-10 03:06:29 -0500

    Adds PCSX2 custom control support. Closes #7

--- ice/emulators/winpcsx2.py
+++ ice/emulators/winpcsx2.py
@@ -28,3 +28,66 @@ class WinPCSX2(downloaded_emulator.DownloadedEmulator,bios_emulator.BiosEmulator
         PCSX2 uses the default command string
         """
         return "\"%s\" \"%s\"" % (self.location,rom.path)
+
+    def path_for_input_file(self):
+        return os.path.join(self.directory,"inis","Lilypad.ini")
+
+    def set_control_scheme(self,controls):
+        """
+        PCSX2 keeps it's control scheme in a flat file for it's input plugin.
+        Since we are using Lilypad, this file is called Lilypad.ini.
+        """
+        # Lilypad.ini is much more complicated than most of the flat files I've
+        # had to deal with. For now, I am just going to worry about letting the
+        # user set their preferences on the xbox360 controller. At some point
+        # I will have to look into a system that allows the user to use any
+        # controller with PCSX2
+        def replacement_function(line):
+            rf = replacement_function
+            # Start of a section
+            if line.startswith("["):
+                rf.section = line.lstrip("[").rstrip().rstrip("]")
+            if rf.section == "Device 13" and line.startswith("Binding"):
+                # Figure out the current binding settings
+                binding_prefix = line[:line.index('=')+1]
+                binding = line[len(binding_prefix):].split(",")
+                control_id = self.ini_number_to_control_identifier(binding[2])
+                new_value = controls[control_id]
+                binding[0] = new_value
+                return "%s%s" % (binding_prefix,",".join(binding))
+            return line
+        replacement_function.section = ""
+        self.replace_contents_of_file(self.path_for_input_file(),replacement_function)
+
+    def ini_number_to_control_identifier(self,ini_number):
+        """
+        Lilypad stores a list of bindings, and the button that is associated
+        with a given binding is based off of a number.
+
+        This function returns a map whose keys are the number, and whose values
+        are the associated button identifier in controls.txt
+        """
+        return {
+            "16":"select",
+            "19":"start",
+            "20":"up",
+            "21":"right",
+            "22":"down",
+            "23":"left",
+            "24":"l2",
+            "25":"r2",
+            "26":"l1",
+            "27":"r1",
+            "28":"triangle",
+            "29":"circle",
+            "30":"x",
+            "31":"square",
+            "32":"leftanalog-up",
+            "33":"leftanalog-right",
+            "34":"leftanalog-down",
+            "35":"leftanalog-left",
+            "36":"rightanalog-up",
+            "37":"rightanalog-right",
+            "38":"rightanalog-down",
+            "39":"rightanalog-left"
+        }[ini_number.strip()]
