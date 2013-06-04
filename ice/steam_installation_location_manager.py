725d60575f8a23805528f6325bfef91be6cad2d2
Scott Rice <meris608@gmail.com>
2013-06-03 21:33:56 -0700

    Uses registry for Steam Location. Fixes #24
    Apparently, Steam stores the path to itself in the registry. This makes
    sense given that Steam needs to be able to find its installation
    directory for the same reason I do, and it would be super easy for the
    installer to just throw the location in the registry. I digress, Steam
    stores its location in the registry. Previously, I required the user to
    enter the location of their Steam installation. This commit removes that
    requirements and gets rid of the location option in config.txt entirely

--- ice/steam_installation_location_manager.py
+++ ice/steam_installation_location_manager.py
@@ -20,18 +20,17 @@ from error.config_error import ConfigError
 # Used to find the shortcuts.vdf file
 osx_userdata_directory = "~/Library/Application Support/Steam/userdata/"
 
-def steam_location():
-    location = settings.config()["Steam"]["location"]
-    if not os.path.exists(os.path.join(location,"Steam.exe")):
-        raise ConfigError("Steam was not found at the given location.","[Steam] Location")
-    return settings.config()["Steam"]["location"]
+def windows_steam_location():
+    import _winreg as registry
+    key = registry.CreateKey(registry.HKEY_CURRENT_USER,"Software\Valve\Steam")
+    return registry.QueryValueEx(key,"SteamPath")[0]
 
 def steam_userdata_location():
     platform = settings.platform_string()
     if platform == "Windows":
         # On Windows, the userdata directory is the steam installation directory
         # with 'userdata' appeneded
-        return os.path.join(steam_location(),"userdata")
+        return os.path.join(windows_steam_location(),"userdata")
     elif platform == "OSX":
         # I'm pretty sure the user can't change this on OS X. I think it always
         # goes to the same location
