36660184c80fb145045c457f697e5f94bc3eca23
Scott Rice <meris608@gmail.com>
2013-08-14 15:18:04 -0500

    Removes unused code from emulator manager

--- ice/emulator_manager.py
+++ ice/emulator_manager.py
@@ -15,56 +15,6 @@ from emulators import *
 from error.config_error import ConfigError
 from ice_logging import log_file, log_user
 
-# List of emulators to use for each conole
-#
-# I was originally going to include a Dreamcast emulator, but none of the
-# emulators that I looked at seemed good. The sound especially was super
-# buggy. It was most likely because I was on Windows 7, but I want to provide
-# a good experience to Win7 as well, so I won't support Dreamcast for now.
-#
-# I was also originally going to include a Wii emulator, since Dolphin already
-# includes Wii support, but it occured to me that the main use case for this
-# application was using an Xbox 360 controller with Steam Big Picture, and
-# since Wii emulation would require a Wiimote, I figured it didn't make sense
-# to include it.
-platform = settings.platform_string()
-if platform == "Windows":
-    emulators = {
-        "NES":          winbsnes.Winbsnes("NES"),
-        "SNES":         winbsnes.Winbsnes("SNES"),
-        "N64":          winproject64.WinProject64("N64"),
-        "Gamecube":     windolphin.WinDolphin("Gamecube"),
-        "PS1":          winepsxe.WinePSXe("PS1"),
-        "PS2":          winpcsx2.WinPCSX2("PS2"),
-        "Genesis":      wingens.WinGens("Genesis"),
-        "Gameboy":      winbsnes.Winbsnes("Gameboy"),
-        "GBA":          winvisualboyadvance.WinVisualBoyAdvance("GBA"),
-    }
-if platform == "OSX":
-    emulators = {
-        "NES":          macmednafen.MacMednafen("NES"),
-        "SNES":         macsnes9x.MacSnes9x("SNES"),
-        # "N64":          ,
-        # "Gamecube":     ,
-        # "PS1":          ,
-        # "PS2":          ,
-        "Genesis":      macmednafen.MacMednafen("Genesis"),
-        "Gameboy":      macmednafen.MacMednafen("Gameboy"),
-        "GBA":          macmednafen.MacMednafen("GBA"),
-    }
-if platform == "Linux":
-    emulators = {
-        # "NES":          ,
-        # "SNES":         ,
-        # "N64":          ,
-        # "Gamecube":     ,
-        # "PS1":          ,
-        # "PS2":          ,
-        # "Genesis":      ,
-        # "Gameboy":      ,
-        # "GBA":          ,
-    }
-
 def emulator_platform_prefix(platform):
     return {
         "Windows":  "Win",
