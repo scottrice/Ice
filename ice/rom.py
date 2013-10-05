e2d15b54dd44d5a4c08f58b255406f29c02b315a
marcusmoller <marcus@powrtoch.org>
2013-10-05 14:01:32 +0200

    Added fix for #60 and #61.

--- ice/rom.py
+++ ice/rom.py
@@ -17,6 +17,7 @@ ROMs
 import sys
 import os
 import stat
+import unicodedata
 
 import settings
 import filesystem_helper
@@ -33,8 +34,12 @@ class ROM:
     def __eq__(self,other):
         return self.path == other.path and self.console == other.console
         
-    def name(self):
+    def name(self): 
         name_with_ext = os.path.basename(self.path)
+
+        # normalize the name to get rid of symbols that break the shortcuts.vdf
+        name_with_ext = unicodedata.normalize('NFKD', unicode(name_with_ext.decode('utf-8'))).encode('ascii', 'ignore')
+
         dot_index = name_with_ext.rfind('.')
         if dot_index == -1:
             # There is no period, so there is no extension. Therefore, the
