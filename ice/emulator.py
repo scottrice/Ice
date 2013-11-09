04ceef66d5aa3ca811deb0404a896d22447e584f
Scott Rice <meris608@gmail.com>
2013-11-09 00:24:19 -0600

    Continues removing unused code from emulator.py

--- ice/emulator.py
+++ ice/emulator.py
@@ -16,18 +16,7 @@ import os
 import tempfile
 import shutil
 
-import abc
-
 class Emulator(object):
-    __metaclass__ = abc.ABCMeta
-    
-    # directory should be set by any subclasses which manage where emulators
-    # are stored (such as the Downloaded Emulator class)
-    directory = ""
-    
-    # Location should be set by any subclasses which manage where emulators
-    # are stored (such as the Downloaded Emulator class)
-    location = ""
         
     def __init__(self, name, location, format):
         self.name = name
@@ -62,25 +51,3 @@ class Emulator(object):
         as the 'StartDir' option of a Steam Shortcut
         """
         return os.path.dirname(self.location)
-        
-    def replace_contents_of_file(self,file_path,replacement_function):
-        """
-        Replaces the contents of a file with the results of replacement_function
-        
-        'replacement_function' is a function which takes a line of input and
-        returns another line, which gets put in the new file.
-        """
-        # Code mainly taken from:
-        # http://stackoverflow.com/questions/39086/search-and-replace-a-line-in-a-file-in-python
-        fh, abs_path = tempfile.mkstemp()
-        new_file = open(abs_path,'w')
-        old_file = open(file_path)
-        for line in old_file:
-            new_file.write(replacement_function(line))
-        # Close handles
-        new_file.close()
-        os.close(fh)
-        old_file.close()
-        # Replace file_path with new file
-        os.remove(file_path)
-        shutil.move(abs_path, file_path)
