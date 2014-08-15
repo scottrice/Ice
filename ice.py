629979c8c9c7e1724ef0403a89d43295372acd02
Scott Rice <srice@fb.com>
2014-08-14 23:41:53 -0700

    Fixes the build break

--- ice.py
+++ ice.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+import traceback
 
-from ice.runners import *
+try:
+    from ice.runners import command_line_runner
 
-if __name__ == "__main__":
-  runner = command_line_runner.CommandLineRunner()
-  runner.run(sys.argv)
+    if __name__ == "__main__":
+        runner = command_line_runner.CommandLineRunner()
+        runner.run(sys.argv)
+except Exception:
+    sys.stdout = open('stdout.log', 'w')
+    sys.stderr = open('stderr.log', 'w')
+    traceback.print_exc()
+    raw_input()
