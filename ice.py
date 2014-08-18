983dbda26f5fc2d4cf91bac44b1f585b77446342
Scott Rice <srice@fb.com>
2014-08-18 08:32:15 -0700

    Changes the entry points to be more descriptive and useful

--- ice.py
+++ ice.py
@@ -10,7 +10,10 @@ try:
         runner = command_line_runner.CommandLineRunner()
         runner.run(sys.argv)
 except Exception:
-    sys.stdout = open('stdout.log', 'w')
-    sys.stderr = open('stderr.log', 'w')
-    traceback.print_exc()
+    stderr = sys.stderr
+    with open('stderr.log', 'w') as f:
+      sys.stderr = f
+      traceback.print_exc()
+      sys.stderr = stderr
+    print "A fatal error occurred. Check `stderr.log` for more details"
     raw_input()
