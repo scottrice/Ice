8373eecc3513bee57b78998d24559058f6a56222
Scott Rice <srice@fb.com>
2014-08-21 08:20:43 -0700

    Removes memoization util since no one uses it now

--- src/ice/utils.py
+++ src/ice/utils.py
@@ -49,34 +49,3 @@ def is_osx():
 
 def is_linux():
     return str(sys.platform).startswith('lin')
-
-# Decorator for memoization
-# Copied from https://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
-class memoize(object):
-    '''Decorator. Caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned
-    (not reevaluated).
-    '''
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args):
-        if not isinstance(args, collections.Hashable):
-            # uncacheable. a list, for instance.
-            # better to not cache than blow up.
-            return self.func(*args)
-        if args in self.cache:
-            return self.cache[args]
-        else:
-            value = self.func(*args)
-            self.cache[args] = value
-            return value
-
-    def __repr__(self):
-        '''Return the function's docstring.'''
-        return self.func.__doc__
-
-    def __get__(self, obj, objtype):
-        '''Support instance methods.'''
-        return functools.partial(self.__call__, obj)
