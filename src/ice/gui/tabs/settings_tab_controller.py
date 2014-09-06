75b26bf8309e306d84b6ad82a01f4a0ccfbda0ac
Yale Thomas <yale.thomas@rd.io>
2014-09-05 20:03:02 -0700

    Combined two rows of each part of the settings page

--- src/ice/gui/tabs/settings_tab_controller.py
+++ src/ice/gui/tabs/settings_tab_controller.py
@@ -15,14 +15,17 @@ class SettingsTabController(object):
 
     self.widget.romsDirectoryDisplayWidget.setText(self.config.roms_directory())
     self.widget.romsButton.setText("Change directory from " + self.config.roms_directory())
+    self.widget.romsDirectoryDisplayWidget.setText("ROMs Directory: " + self.config.roms_directory())
     self.widget.romsButton.clicked.connect(self.romDirDialog)
 
     self.widget.backupDirectoryDisplayWidget.setText(self.config.backup_directory())
     self.widget.backupButton.setText("Change directory from " + self.config.backup_directory())
+    self.widget.backupDirectoryDisplayWidget.setText("Backup Directory: " + self.config.backup_directory())
     self.widget.backupButton.clicked.connect(self.backupDirDialog)
 
     self.widget.userdataDirectoryDisplayWidget.setText("Change directory from " + self.config.userdata_directory())
     self.widget.userdataButton.setText("Change directory from " + self.config.userdata_directory())
+    self.widget.userdataDirectoryDisplayWidget.setText("Steam Userdata Directory: " + self.config.userdata_directory())
     self.widget.userdataButton.clicked.connect(self.userdataDirDialog)
 
     if self.config.steam_userdata_location():
@@ -32,17 +35,17 @@ class SettingsTabController(object):
   def romDirDialog(self):
     dir = QtGui.QFileDialog.getExistingDirectory(self.widget, "Pick Roms Directory", self.config.roms_directory())
     self.config.set_roms_directory(str(dir))
-    self.widget.romsDirectoryDisplayWidget.setText(self.config.roms_directory())
+    self.widget.romsDirectoryDisplayWidget.setText("ROMs Directory: " + self.config.roms_directory())
     self.widget.romsButton.setText("Change directory from " + self.config.roms_directory())
 
   def backupDirDialog(self):
     dir = QtGui.QFileDialog.getExistingDirectory(self.widget, "Pick Backup Directory", self.config.backup_directory())
     self.config.set_backup_directory(str(dir))
-    self.widget.backupDirectoryDisplayWidget.setText(self.config.backup_directory())
+    self.widget.backupDirectoryDisplayWidget.setText("Backup Directory: " + self.config.backup_directory())
     self.widget.backupButton.setText("Change directory from " + self.config.backup_directory())
 
   def userdataDirDialog(self):
     dir = QtGui.QFileDialog.getExistingDirectory(self.widget, "Pick Userdata Directory", self.config.userdata_directory())
     self.config.set_userdata_directory(str(dir))
-    self.widget.userdataDirectoryDisplayWidget.setText("Change directory from " + self.config.userdata_directory())
+    self.widget.userdataDirectoryDisplayWidget.setText("Steam Userdata Directory: " + self.config.userdata_directory())
     self.widget.userdataButton.setText("Change directory from " + self.config.userdata_directory())
