9c41d3ba78bbc7b858efa398e85261442d3e56fa
Scott Rice <srice@fb.com>
2014-12-23 14:38:33 -0500

    Refactors Console to use the new Filesystem class

--- src/ice/console.py
+++ src/ice/console.py
@@ -23,8 +23,9 @@ from emulator import Emulator
 from rom import ROM
 
 class Console(BackedObject):
-    def __init__(self, backing_store, identifier, config):
+    def __init__(self, backing_store, identifier, config, filesystem):
         super(Console, self).__init__(backing_store, identifier)
+        self.filesystem             = filesystem
         self.config                 = config
 
         self.fullname               = identifier
@@ -78,20 +79,14 @@ class Console(BackedObject):
         console
         """
         roms = []
-        if not os.path.exists(self.roms_directory()):
+        if not self.filesystem.path_exists(self.roms_directory()):
             # TODO: Tell the user when we are about to create a directory for them
             # TODO: Why the hell are we checking for and creating directories inside of `find_roms`?
             os.makedirs(self.roms_directory())
-        for filename in os.listdir(self.roms_directory()):
-            file_path = os.path.join(self.roms_directory(),filename)
-            if not os.path.isdir(file_path):
-                # On Linux/OSX, we want to make sure hidden files don't get
-                # accidently added as well
-                if not utils.is_windows() and filename.startswith('.'):
-                    continue
-                if self.emulator is not None and not self.is_valid_rom(file_path):
-                    # TODO: Tell the user that we are ignoring this file
-                    # TODO: Again, why the hell are we logging that here?????
-                    continue
-                roms.append(ROM(file_path,self))
+        for file_path in self.filesystem.files_in_directory(self.roms_directory()):
+            if self.emulator is not None and not self.is_valid_rom(file_path):
+                # TODO: Tell the user that we are ignoring this file
+                # TODO: Again, why the hell are we logging that here?????
+                continue
+            roms.append(ROM(file_path,self))
         return roms
--- src/ice/persistence/console_manager.py
+++ src/ice/persistence/console_manager.py
@@ -7,11 +7,13 @@ Copyright (c) 2014 Scott Rice. All rights reserved.
 
 from backed_object_manager import BackedObjectManager
 from ice.console import Console
+from ice.filesystem import Filesystem
 
 class ConsoleManager(BackedObjectManager):
   def __init__(self, backing_store, config):
     super(ConsoleManager, self).__init__(backing_store)
     self.config = config
+    self.filesystem = Filesystem()
 
   def new(self, identifier):
-    return Console(self.backing_store, identifier, self.config)
+    return Console(self.backing_store, identifier, self.config, self.filesystem)
