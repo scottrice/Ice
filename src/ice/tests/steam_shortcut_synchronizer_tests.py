2546472d4e3a06f70fc75d16a4b2f7bd36905734
Scott Rice <srice@fb.com>
2014-12-29 05:51:30 -0600

    Adds a test to ensure that the Shortcut Synchronizer logs when a ROM is added/deleted

--- src/ice/tests/steam_shortcut_synchronizer_tests.py
+++ src/ice/tests/steam_shortcut_synchronizer_tests.py
@@ -94,3 +94,41 @@ class SteamShortcutSynchronizerTests(unittest.TestCase):
     self.assertIn(shortcut2, new_shortcuts)
     self.assertIn(shortcut3, new_shortcuts)
     self.assertIn(shortcut4, new_shortcuts)
+
+  def test_sync_roms_for_user_logs_when_a_rom_is_added(self):
+    shortcut = Shortcut("Game", "/Path/to/game", "/Path/to", "", ICE_FLAG_TAG)
+    rom = mock.MagicMock()
+    rom.to_shortcut.return_value = shortcut
+
+    self.synchronizer.sync_roms_for_user(self.mock_user, [rom])
+    self.assertTrue(self.mock_logger.info.called)
+
+  def test_sync_roms_for_user_logs_once_for_each_added_rom(self):
+    shortcut1 = Shortcut("Game1", "/Path/to/game1", "/Path/to", "", ICE_FLAG_TAG)
+    rom1 = mock.MagicMock()
+    rom1.to_shortcut.return_value = shortcut1
+    shortcut2 = Shortcut("Game2", "/Path/to/game2", "/Path/to", "", ICE_FLAG_TAG)
+    rom2 = mock.MagicMock()
+    rom2.to_shortcut.return_value = shortcut2
+    shortcut3 = Shortcut("Game3", "/Path/to/game3", "/Path/to", "", ICE_FLAG_TAG)
+    rom3 = mock.MagicMock()
+    rom3.to_shortcut.return_value = shortcut3
+
+    self.synchronizer.sync_roms_for_user(self.mock_user, [rom1, rom2, rom3])
+    self.assertEquals(self.mock_logger.info.call_count, 3)
+
+  def test_sync_roms_for_user_logs_when_a_rom_is_removed(self):
+    shortcut = Shortcut("Game", "/Path/to/game", "/Path/to", "", ICE_FLAG_TAG)
+    self.mock_user.shortcuts = [shortcut]
+
+    self.synchronizer.sync_roms_for_user(self.mock_user, [])
+    self.assertTrue(self.mock_logger.info.called)
+
+  def test_sync_roms_for_user_logs_once_for_each_removed_rom(self):
+    shortcut1 = Shortcut("Game1", "/Path/to/game1", "/Path/to", "", ICE_FLAG_TAG)
+    shortcut2 = Shortcut("Game2", "/Path/to/game2", "/Path/to", "", ICE_FLAG_TAG)
+    shortcut3 = Shortcut("Game3", "/Path/to/game3", "/Path/to", "", ICE_FLAG_TAG)
+    self.mock_user.shortcuts = [shortcut1, shortcut2, shortcut3]
+
+    self.synchronizer.sync_roms_for_user(self.mock_user, [])
+    self.assertEquals(self.mock_logger.info.call_count, 3)
