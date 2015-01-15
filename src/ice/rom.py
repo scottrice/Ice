bde5170af26347174f45ce2de1ef98ddacc724ed
Florian Piesche <florian@yellowkeycard.net>
2015-01-15 19:29:03 +0000

    Fix linter errors and comment out unused imports.

--- src/ice/rom.py
+++ src/ice/rom.py
@@ -8,38 +8,42 @@ Copyright (c) 2012 Scott Rice. All rights reserved.
 
 ROM model. Handles the collection of data that makes up a ROM (as of right now,
 just the path to the ROM and what console is is from). Also contains some
-convenience methods for the filesystem. 
+convenience methods for the filesystem.
 
 Functionality should be added to this class if it heavily involves the use of
 ROMs
 """
 
-import sys
+# import sys
 import os
 import re
-import stat
+# import stat
 import unicodedata
 
 from pysteam.shortcut import Shortcut
 
 ICE_FLAG_TAG = "~ManagedByIce"
 
-class ROM:
-    def __init__(self,path,console):
+class ROM(object):
+    """ Class representing an emulated game image. """
+
+    def __init__(self, path, console):
         self.path = path
         self.console = console
 
     def __repr__(self):
         return self.name()
 
-    def __eq__(self,other):
+    def __eq__(self, other):
         return self.path == other.path and self.console == other.console
-        
+
     def name(self):
+        """ Return the filename for the ROM. """
         name_with_ext = os.path.basename(self.path)
 
         # normalize the name to get rid of symbols that break the shortcuts.vdf
-        name_with_ext = unicodedata.normalize('NFKD', unicode(name_with_ext.decode('utf-8'))).encode('ascii', 'ignore')
+        name_with_ext = unicodedata.normalize('NFKD',\
+            unicode(name_with_ext.decode('utf-8'))).encode('ascii', 'ignore')
 
         dot_index = name_with_ext.rfind('.')
         if dot_index == -1:
@@ -50,6 +54,7 @@ class ROM:
         return name_with_ext[:dot_index]
 
     def prefixed_name(self, clean=False):
+        """ Return the ROM name prefixed with the console shorthand. """
         prefix = self.console.prefix
         name = self.clean_name() if clean else self.name()
         if prefix:
@@ -58,13 +63,15 @@ class ROM:
             return name
 
     def clean_name(self):
-        return re.sub("(\(.*\)|(\[.*\])", "", self.name()).strip()
-        
+        """ Return the ROM name stripped of GoodTools tags. """
+        return re.sub(r"(\(.*\)|(\[.*\])", "", self.name()).strip()
+
     def to_shortcut(self):
+        """ Create a pysteam Shortcut object from the ROM object. """
         appname = self.prefixed_name(clean=True)
         exe = self.console.emulator.command_string(self)
         startdir = self.console.emulator.startdir(self)
         icon = self.console.icon
         category = self.console.fullname
-        tags = [ ICE_FLAG_TAG, category ]
-        return Shortcut(appname,exe,startdir,icon,tags)
+        tags = [ICE_FLAG_TAG, category]
+        return Shortcut(appname, exe, startdir, icon, tags)
