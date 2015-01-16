40704cd5fe025f80ad0fb9b70b9d7259f3b6ad6a
Florian Piesche <florian@yellowkeycard.net>
2015-01-16 00:19:25 +0000

    Fix linter errors and comment out unused imports. Move hidden file check to is_valid_rom(). Rework directory crawl in find_roms() for efficiency. Remove os.makedirs(self.rom_directory()) - little point in doing that.

--- src/ice/console.py
+++ src/ice/console.py
@@ -19,11 +19,12 @@ import os
 import filesystem_helper
 import utils
 from persistence.backed_object import BackedObject
-from persistence.config_file_backing_store import ConfigFileBackingStore
-from emulator import Emulator
+# from persistence.config_file_backing_store import ConfigFileBackingStore
+# from emulator import Emulator
 from rom import ROM
 
 class Console(BackedObject):
+
     def __init__(self, backing_store, identifier, config):
         super(Console, self).__init__(backing_store, identifier)
         self.config                 = config
@@ -63,40 +64,41 @@ class Console(BackedObject):
         """
         if self.custom_roms_directory:
             return self.custom_roms_directory
-        return os.path.join(self.config.roms_directory(),self.shortname)
-      
-    def is_valid_rom(self,path):
+        return os.path.join(self.config.roms_directory(), self.shortname)
+
+    def is_valid_rom(self, path):
         """
         This function determines if a given path is actually a valid ROM file.
-        If a list of extensions is supplied for this console, we check if the path has a valid extension
-        If no extensions are defined for this console, we just accept any file
+        If a list of extensions is supplied for this console, we check if the
+            path has a valid extension.
+        If no extensions are defined for this console, we just accept any file.
+        Hidden files are ignored.
         """
+
+        if os.path.basename().startswith("."):
+            # hidden file
+            return False
 
         if self.extensions == "":
             return True
         extension = os.path.splitext(path)[1].lower()
-        return any(extension == ('.'+x.strip().lower()) for x in self.extensions.split(','))
-  
+        return any(extension == ('.' + x.strip().lower()) for x in\
+            self.extensions.split(','))
+
     def find_roms(self):
         """
-        Reads a list of all the ROMs from the appropriate directory for the
+        Build a list of all the ROMs from the appropriate directory for the
         console
         """
         roms = []
-        if not os.path.exists(self.roms_directory()):
-            # TODO: Tell the user when we are about to create a directory for them
-            # TODO: Why the hell are we checking for and creating directories inside of `find_roms`?
-            os.makedirs(self.roms_directory())
-        for filename in os.listdir(self.roms_directory()):
-            file_path = os.path.join(self.roms_directory(),filename)
-            if not os.path.isdir(file_path):
-                # On Linux/OSX, we want to make sure hidden files don't get
-                # accidently added as well
-                if not utils.is_windows() and filename.startswith('.'):
-                    continue
-                if self.emulator is not None and not self.is_valid_rom(file_path):
+        for root, _, files in os.walk(self.roms_directory(), followlinks=True):
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                if self.emulator is not None and self.is_valid_rom(file_path):
+                    # append valid ROMs to rom list, skip other files
+                    roms.append(ROM(file_path, self))
+                else:
                     # TODO: Tell the user that we are ignoring this file
                     # TODO: Again, why the hell are we logging that here?????
                     continue
-                roms.append(ROM(file_path,self))
         return roms
