scottrice/Ice

commit 792e7609fc96f47975547aa088e6618fef13c2d4

Remove Backing Store logic from Console, make it a dumb value object

src/ice/persistence/adapters/console_adapter.py
@@ -7,7 +7,31 @@ class ConsoleBackedObjectAdapter(object):
     self.emulators = emulators
 
   def new(self, backing_store, identifier):
-    return Console(backing_store, identifier, self.emulators)
+    fullname = identifier
+    shortname = backing_store.get(identifier, 'nickname', self.fullname)
+    extensions = backing_store.get(identifier, 'extensions', "")
+    custom_roms_directory = backing_store.get(identifier, 'roms directory', "")
+    prefix = backing_store.get(identifier, 'prefix', "")
+    icon = backing_store.get(identifier, 'icon', "")
+    images_directory = backing_store.get(identifier, 'images directory', "")
+    emulator_identifier = backing_store.get(identifier, 'emulator', "")
+
+    icon = os.path.expanduser(icon)
+    custom_roms_directory = os.path.expanduser(custom_roms_directory)
+    images_directory = os.path.expanduser(images_directory)
+
+    emulator = self.emulators.find(emulator_identifier)
+
+    return Console(
+      fullname,
+      shortname,
+      extensions,
+      custom_roms_directory,
+      prefix,
+      icon,
+      images_directory,
+      emulator,
+    )
 
   def save_in_store(self, backing_store, identifier, console):
     backing_store.set(identifier, 'nickname', console.shortname)

src/ice/console.py
@@ -16,32 +16,20 @@ their emulators. This includes finding a list of ROMs in this console's folder.
 
 import os
 
-import utils
-from persistence.backed_object import BackedObject
-from persistence.config_file_backing_store import ConfigFileBackingStore
-from emulator import Emulator
 from rom import ROM
 
 
-class Console(BackedObject):
+class Console(object):
 
-  def __init__(self, backing_store, identifier, emulators):
-    super(Console, self).__init__(backing_store, identifier)
-
-    self.fullname = identifier
-    self.shortname = self.backed_value('nickname', self.fullname)
-    self.extensions = self.backed_value('extensions', "")
-    self.custom_roms_directory = self.backed_value('roms directory', "")
-    self.prefix = self.backed_value('prefix', "")
-    self.icon = self.backed_value('icon', "")
-    self.images_directory = self.backed_value('images directory', "")
-    self.emulator_identifier = self.backed_value('emulator', "")
-
-    self.icon = os.path.expanduser(self.icon)
-    self.custom_roms_directory = os.path.expanduser(self.custom_roms_directory)
-    self.images_directory = os.path.expanduser(self.images_directory)
-
-    self.emulator = emulators.find(self.emulator_identifier)
+  def __init__(self, fullname, shortname, extensions, custom_roms_directory, prefix, icon, images_directory, emulator):
+    self.fullname = fullname
+    self.shortname = shortname
+    self.extensions = extensions
+    self.custom_roms_directory = custom_roms_directory
+    self.prefix = prefix
+    self.icon = icon
+    self.images_directory = images_directory
+    self.emulator = emulator
 
   def __repr__(self):
     return self.fullname

commit 295bda9ae0e8d443bbe9948b40ac38417a1db381

Fix runtime errors introduced in 792e760.

src/ice/persistence/adapters/console_adapter.py
@@ -1,4 +1,6 @@
 
+import os
+
 from ice.console import Console
 
 class ConsoleBackedObjectAdapter(object):
@@ -8,7 +10,7 @@ class ConsoleBackedObjectAdapter(object):
 
   def new(self, backing_store, identifier):
     fullname = identifier
-    shortname = backing_store.get(identifier, 'nickname', self.fullname)
+    shortname = backing_store.get(identifier, 'nickname', fullname)
     extensions = backing_store.get(identifier, 'extensions', "")
     custom_roms_directory = backing_store.get(identifier, 'roms directory', "")
     prefix = backing_store.get(identifier, 'prefix', "")