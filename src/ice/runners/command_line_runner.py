4fb253043d9b4841893bd8fd39bf27efee64c844
Scott Rice <srice@fb.com>
2015-07-26 13:32:17 -0700

    Allow passing in config/consoles/emulators.txt locations from command line
    Summary: This will be really useful for Integration tests, along with whenever we get the desktop app running (since it will communicate with Ice via the command line)
    
    Test Plan: Run `src/ice.py --config=/Path/to/different/config`, where the different config has a special backups directory location. Confirm that running Ice adds a backup to this location.

--- src/ice/runners/command_line_runner.py
+++ src/ice/runners/command_line_runner.py
@@ -5,14 +5,27 @@ Created by Scott on 2014-08-14.
 Copyright (c) 2014 Scott Rice. All rights reserved.
 """
 
+import argparse
+
 from ice_engine import IceEngine
 
 
 class CommandLineRunner(object):
 
+  def get_command_line_args(self, argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', type=str, default=None)
+    parser.add_argument('-C', '--consoles', type=str, default=None)
+    parser.add_argument('-e', '--emulators', type=str, default=None)
+    return parser.parse_args(argv)
+
   def run(self, argv):
-    # TODO: Configure IceEngine based on the contents of argv
-    engine = IceEngine()
+    options = self.get_command_line_args(argv[1:])
+    engine = IceEngine(
+      config_override=options.config,
+      consoles_override=options.consoles,
+      emulators_override=options.emulators,
+    )
     engine.run()
     # Keeps the console from closing (until the user hits enter) so they can
     # read any console output
