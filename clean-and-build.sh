7c7ed527a60a94c9cd693be452b3635518018781
ProfessorKaos64 <mdeguzis@gmail.com>
2016-01-09 10:42:04 -0500

    add manual clean and build scipt

---
+++ clean-and-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Cleans project files for manual build attempts and builds
+
+# uninstall python applications
+sudo pip remove  pysteam appdirs
+
+# Remove project files
+sudo rm -rf /tmp/pip_build_root
+sudo rm -rf build
+sudo rm -rf Ice.egg-info
+sudo rm -rf dist
+
+# Attemp new project build
+sudo pip install --upgrade pysteam appdirs
+sudo python setup.py install
+
+# Run info
+echo -e "\nAttempt to run 'python -m ice'\n"
