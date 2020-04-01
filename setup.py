4882aba9d0cc554c0dafb87c2a83f3c71133ffeb
ipetrovski <ipetrovski81@gmail.com>
2020-04-01 10:09:50 -0400

    fixed pip.rep import for pip version >= 10

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ import sys
 import uuid
 
 from distutils.sysconfig import get_python_lib
-from pip.req import parse_requirements
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
 def is_windows(platform):
